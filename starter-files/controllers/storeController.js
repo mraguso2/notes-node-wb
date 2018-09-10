052414d969e497226c7299603be69a3a26bca7b5
Michael Raguso <michael.raguso@gmail.com>
2018-09-09 21:05:15 -0400

    flash messages, query db for stores, edit/update stores

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,3 +1,7 @@
+const mongoose = require('mongoose');
+
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
   console.log(req.name);
   res.render('index');
@@ -7,7 +11,35 @@ exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
 };
 
-exports.createStore = (req, res) => {
-  console.log(req.body);
-  res.json(req.body);
+exports.createStore = async (req, res) => {
+  const store = await (new Store(req.body)).save(); // so we get back the slug
+  req.flash('success', `Successsfully Created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  // 1. Query the database for a list of all stores
+  const stores = await Store.find();
+  // sends stores to the template
+  res.render('stores', { title: 'Stores', stores });
+};
+
+exports.editStore = async (req, res) => {
+  // 1. Find the store given the ID
+  const store = await Store.findOne({ _id: req.params.id });
+  // 2. confirm they are the owner of the store
+  // TODO
+  // 3. render out the edit form so the user can update their store
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  // find and update the store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the new store instead of the old one
+    runValidators: true
+  }).exec();
+  // redirect them to the store and tell them it worked
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store ➔</a>`);
+  res.redirect(`/stores/${store._id}/edit`);
 };
--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 
+// fn composition to handle errors with asnyc/await
+const { catchErrors } = require('../handlers/errorHandlers');
+
 // Do work here - shell off work to controllers folder. Just put routes in here
-router.get('/', storeController.homePage);
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
-router.post('/add', storeController.createStore);
+router.post('/add', catchErrors(storeController.createStore));
+router.post('/add/:id', catchErrors(storeController.updateStore));
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 
 module.exports = router;
